d7d8c8acd1957d9dc784050f843762cba1178d5b
MiguelMA-GH <al269436@uji.es>
2019-05-13 16:26:23 +0200

    Instructor
    Ahora creo que si

--- src/main/java/es/uji/ei1027/controller/InstructorController.java
+++ src/main/java/es/uji/ei1027/controller/InstructorController.java
@@ -5,7 +5,6 @@ import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -61,9 +60,16 @@ public class InstructorController {
     public String processUpdateSubmit(@PathVariable String dni, 
                             @ModelAttribute("instructor") Instructor instructor, 
                             BindingResult bindingResult) {
+	 	InstructorValidator instructorValidator = new InstructorValidator(); 
+		instructorValidator.validate(instructor, bindingResult);
          if (bindingResult.hasErrors()) 
              return "instructor/update";
-         instructordao.updateInstructor(instructor);
+         try {
+     		instructordao.updateInstructor(instructor);
+     	} catch (DuplicateKeyException e) {
+     		bindingResult.rejectValue("dni", "obligatorio","El dni ya existe");
+     		return "instructor/add";
+     	}
          return "redirect:../listarInstructores"; 
     }
 	
--- src/main/java/es/uji/ei1027/dao/InstructorDao.java
+++ src/main/java/es/uji/ei1027/dao/InstructorDao.java
@@ -6,7 +6,6 @@ import java.util.List;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataAccessException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
